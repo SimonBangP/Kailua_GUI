711c81889543a5c26509d9971ff7b3ca8621ca68
OmerMert32 <omermert@live.dk>
2022-04-21 17:12:59 +0200

    lidt mere customer done

---
+++ src/main/java/com/example/kailua_gui/Controller/CustomerController.java
@@ -0,0 +1,12 @@
+package com.example.kailua_gui.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CustomerController {
+
+    @GetMapping("/customer")
+    public String Customer () {
+        return "customer";}
+}
