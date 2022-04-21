32520baf5c6ce652d8d449df10d8abf502189200
simonbang <simo12k1@stud.kea.dk>
2022-04-21 09:03:26 +0200

    Initial commit

---
+++ src/main/java/com/example/kailua_gui/KailuaGuiApplication.java
@@ -0,0 +1,13 @@
+package com.example.kailua_gui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KailuaGuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KailuaGuiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kailua_gui/KailuaGuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kailua_gui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KailuaGuiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
