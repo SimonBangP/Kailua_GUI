8186b7ab4aa0396c6b0992d87431315a1dd0e367
OmerMert32 <omermert@live.dk>
2022-04-21 17:01:32 +0200

    Customerservice done

--- src/main/java/com/example/kailua_gui/Service/CustomerService.java
+++ src/main/java/com/example/kailua_gui/Service/CustomerService.java
@@ -1,4 +1,26 @@
 package com.example.kailua_gui.Service;
 
+import com.example.kailua_gui.Model.Customer;
+import com.example.kailua_gui.Repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CustomerService {
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public List<Customer> getCustomers(){
+        return customerRepository.getCustomers();
+    }
+    public void createNewCustomer(Customer customer){
+        customerRepository.createNewCustomer(customer);
+    }
+    public Customer findSpecificCustomer(int customerId){
+        return customerRepository.findSpecificCustomer(customerId);
+    }
+    public void deleteCustomer(int customerId){ customerRepository.deleteCustomer(customerId);}
+    public void updateCurrentCustomer(Customer customer, int customerId){ customerRepository.updateCurrentCustomer(customer, customerId); }
 }
