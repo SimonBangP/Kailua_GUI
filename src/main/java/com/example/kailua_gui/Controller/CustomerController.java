f2a76b85ab326d90f65d3976277f2d583c1bc658
OmerMert32 <omermert@live.dk>
2022-04-24 17:30:20 +0200

    lidt mere customer done

--- src/main/java/com/example/kailua_gui/Controller/CustomerController.java
+++ src/main/java/com/example/kailua_gui/Controller/CustomerController.java
@@ -1,12 +1,38 @@
 package com.example.kailua_gui.Controller;
 
+import com.example.kailua_gui.Model.Customer;
+import com.example.kailua_gui.Service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
 
+    @Autowired
+    CustomerService customerService;
+
+
     @GetMapping("/customer")
-    public String Customer () {
-        return "customer";}
+    public String customer () {
+        return "customer/customer";}
+
+    @GetMapping ("/createCustomer")
+    public String create() {
+        return "customer/createCustomer";
+    }
+    @PostMapping("createCustomer")
+        public String create(@ModelAttribute Customer customer){
+        customerService.createNewCustomer(customer);
+        return "redirect:/";
+    }
+
+    @GetMapping("deleteCustomer")
+    public String delete(){
+        return "customer/deleteCustomer";
+    }
+
 }
+
