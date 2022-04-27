88b7df042f5f938a832fc92e5ed7348be59d611a
OmerMert32 <omermert@live.dk>
2022-04-27 11:04:41 +0200

    lidt mere customer done

--- src/main/java/com/example/kailua_gui/Controller/FeedbackController.java
+++ src/main/java/com/example/kailua_gui/Controller/FeedbackController.java
@@ -1,29 +1,13 @@
 package com.example.kailua_gui.Controller;
 
-import com.example.kailua_gui.Model.Feedback;
-import com.example.kailua_gui.Service.FeedbackService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FeedbackController {
 
-    @Autowired
-    FeedbackService service;
-
     @GetMapping("/feedback")
     public String getFeedback() {
         return "feedback";
-    }
-
-    @PostMapping("/feedback/create")
-    public ResponseEntity<Feedback> createFeedback(@RequestBody String message) {
-        Feedback feedback = new Feedback(message);
-        service.createFeedback(feedback);
-
-        return new ResponseEntity<>(feedback, HttpStatus.CREATED);
     }
 }
